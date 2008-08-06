a61cc6e17ca0b4793857c22c3b7f053ed4301be0
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2008-08-06 20:28:16 +0000

    more tests

--- trunk/source/src/test/java/com/test/junit/FetchTest.java
+++ trunk/source/src/test/java/com/test/junit/FetchTest.java
@@ -10,27 +10,22 @@ import com.trg.search.Search;
 
 
 public class FetchTest extends TestBase {
-	private PersonDAO personDAO;
 	private GeneralDAO generalDAO;
 
-	public void setPersonDAO(PersonDAO personDAO) {
-		this.personDAO = personDAO;
-	}
-	
 	public void setGeneralDAO(GeneralDAO generalDAO) {
 		this.generalDAO = generalDAO;
 	}
 
 
 	public void testFetchEntity() {
+		//TODO how can we test which entities are being loaded lazily?
 	}
 	
 	public void testFetchOther() {
 		initDB();
 		
 		Search s = new Search(Person.class);
-		s.addFilterEqual("lastName", "Beta");
-		s.addFilterLessThan("age", 20);
+		s.addFilterIn("id", joeA.getId(), margretB.getId());
 		s.addSort("firstName");
 		
 		List<Object[]> resultArray;
@@ -45,7 +40,7 @@ public class FetchTest extends TestBase {
 		resultArray = generalDAO.search(s);
 		assertEquals(2, resultArray.size());
 		assertEquals("Joe", resultArray.get(0)[0]);
-		assertEquals("Beta", resultArray.get(0)[1]);
+		assertEquals("Alpha", resultArray.get(0)[1]);
 		assertEquals(10, resultArray.get(0)[2]);
 		assertEquals("Margret", resultArray.get(1)[0]);
 		assertEquals("Beta", resultArray.get(1)[1]);
@@ -55,7 +50,7 @@ public class FetchTest extends TestBase {
 		resultList = generalDAO.search(s);
 		assertEquals(2, resultList.size());
 		assertEquals("Joe", resultList.get(0).get(0));
-		assertEquals("Beta", resultList.get(0).get(1));
+		assertEquals("Alpha", resultList.get(0).get(1));
 		assertEquals(10, resultList.get(0).get(2));
 		assertEquals("Margret", resultList.get(1).get(0));
 		assertEquals("Beta", resultList.get(1).get(1));
@@ -65,7 +60,7 @@ public class FetchTest extends TestBase {
 		resultMap = generalDAO.search(s);
 		assertEquals(2, resultMap.size());
 		assertEquals("Joe", resultMap.get(0).get("first"));
-		assertEquals("Beta", resultMap.get(0).get("lastName"));
+		assertEquals("Alpha", resultMap.get(0).get("lastName"));
 		assertEquals(10, resultMap.get(0).get("age"));
 		assertEquals("Margret", resultMap.get(1).get("first"));
 		assertEquals("Beta", resultMap.get(1).get("lastName"));
@@ -92,5 +87,48 @@ public class FetchTest extends TestBase {
 		assertEquals(2, resultMap.size());
 		assertEquals("Joe", resultMap.get(0).get("firstName"));
 		assertEquals("Margret", resultMap.get(1).get("firstName"));
+		
+		s.setFetchMode(Search.FETCH_SINGLE);
+		List<String> resultSingle = generalDAO.search(s);
+		assertEquals(2, resultSingle.size());
+		assertEquals("Joe", resultSingle.get(0));
+		assertEquals("Margret", resultSingle.get(1));
+		
+		
+		
+		s.clearFetch();
+		s.addFetch("home.type", "homeType");
+		s.addFetch("father.mother.home.address.street");
+		s.addFetch("firstName", "home.type");
+		
+		s.setFetchMode(Search.FETCH_ARRAY);
+		resultArray = generalDAO.search(s);
+		assertEquals(2, resultArray.size());
+		assertEquals("house", resultArray.get(0)[0]);
+		assertEquals("3290 W Fulton", resultArray.get(0)[1]);
+		assertEquals("Joe", resultArray.get(0)[2]);
+		assertEquals("apartment", resultArray.get(1)[0]);
+		assertEquals(null, resultArray.get(1)[1]);
+		assertEquals("Margret", resultArray.get(1)[2]);
+		
+		s.setFetchMode(Search.FETCH_LIST);
+		resultList = generalDAO.search(s);
+		assertEquals(2, resultList.size());
+		assertEquals("house", resultList.get(0).get(0));
+		assertEquals("3290 W Fulton", resultList.get(0).get(1));
+		assertEquals("Joe", resultList.get(0).get(2));
+		assertEquals("apartment", resultList.get(1).get(0));
+		assertEquals(null, resultList.get(1).get(1));
+		assertEquals("Margret", resultList.get(1).get(2));
+		
+		s.setFetchMode(Search.FETCH_MAP);
+		resultMap = generalDAO.search(s);
+		assertEquals(2, resultMap.size());
+		assertEquals("house", resultMap.get(0).get("homeType"));
+		assertEquals("3290 W Fulton", resultMap.get(0).get("father.mother.home.address.street"));
+		assertEquals("Joe", resultMap.get(0).get("home.type"));
+		assertEquals("apartment", resultMap.get(1).get("homeType"));
+		assertEquals(null, resultMap.get(1).get("father.mother.home.address.street"));
+		assertEquals("Margret", resultMap.get(1).get("home.type"));
 	}
 }
--- trunk/source/src/test/java/com/test/junit/TrickyIssueTest.java
+++ trunk/source/src/test/java/com/test/junit/TrickyIssueTest.java
@@ -1,5 +1,73 @@
 package com.test.junit;
 
-public class TrickyIssueTest extends TestBase {
+import java.util.List;
+import java.util.Map;
 
+import com.test.model.Person;
+import com.trg.dao.GeneralDAO;
+import com.trg.dao.GenericDAO;
+import com.trg.search.Search;
+
+public class TrickyIssueTest extends TestBase {
+	private GeneralDAO generalDAO;
+	
+	public void setGeneralDAO(GeneralDAO generalDAO) {
+		this.generalDAO = generalDAO;
+	}
+
+	/**
+	 * The alias error occurs when using fetch mode FETCH_MAP. It occurs when
+	 * there is a fetch that has a key with no "." in it and is the same as a
+	 * property that is used in a filter.
+	 */
+	public void testAliasError() {
+		initDB();
+		
+		List<Map<String, Object>> resultMap;
+		
+		Search s = new Search(Person.class);
+		s.addFilterEqual("firstName", "Joe");
+		s.addFilterEqual("age", 10);
+		s.addSort("lastName");
+		s.setFetchMode(Search.FETCH_MAP);
+		
+		s.addFetch("firstName");
+		
+		resultMap = generalDAO.search(s);
+		assertEquals(2, resultMap.size());
+		assertEquals("Joe", resultMap.get(0).get("firstName"));
+		assertEquals("Joe", resultMap.get(1).get("firstName"));
+		
+		
+		s.addFetch("lastName");
+		
+		resultMap = generalDAO.search(s);
+		assertEquals(2, resultMap.size());
+		assertEquals("Joe", resultMap.get(0).get("firstName"));
+		assertEquals("Alpha", resultMap.get(0).get("lastName"));
+		assertEquals("Joe", resultMap.get(1).get("firstName"));
+		assertEquals("Beta", resultMap.get(1).get("lastName"));
+		
+		
+		s.clearFetch();
+		s.addFetch("firstName", "firstName");
+		s.addFetch("age"); //this uses age for the property and key
+		s.addFetch("lastName", "Last Name");
+		s.addFetch("mother.lastName");
+		
+		resultMap = generalDAO.search(s);
+		assertEquals(2, resultMap.size());
+		assertEquals("Joe", resultMap.get(0).get("firstName"));
+		assertEquals(10, resultMap.get(0).get("age"));
+		assertEquals("Alpha", resultMap.get(0).get("Last Name"));
+		assertEquals("Alpha", resultMap.get(0).get("mother.lastName"));
+		assertEquals("Joe", resultMap.get(1).get("firstName"));
+		assertEquals(10, resultMap.get(1).get("age"));
+		assertEquals("Beta", resultMap.get(1).get("Last Name"));
+		assertEquals("Beta", resultMap.get(1).get("mother.lastName"));
+	}
+
+	public void testEagerFetchingPagingError() {
+		
+	}
 }
