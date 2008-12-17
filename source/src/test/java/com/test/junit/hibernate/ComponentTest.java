68946e31cabb3d065dc6414b7a02e6239696dc07
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2008-12-17 19:04:24 +0000

    New tests for Hibernate component types.

---
+++ source/src/test/java/com/test/junit/hibernate/ComponentTest.java
@@ -0,0 +1,9 @@
+package com.test.junit.hibernate;
+
+import com.test.misc.HibernateBaseDAOTester;
+
+public class ComponentTest extends com.test.base.ComponentTest {
+	public void setHibernateBaseDAOTester(HibernateBaseDAOTester target) {
+		this.target = target;
+	}
+}
---
+++ source/src/test/java/com/test/model/Name.java
@@ -0,0 +1,25 @@
+package com.test.model;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Name component used for Pets (note, this is not used for Persons)
+ */
+@Embeddable
+public class Name {
+	private String first;
+	private String last;
+	
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+}
