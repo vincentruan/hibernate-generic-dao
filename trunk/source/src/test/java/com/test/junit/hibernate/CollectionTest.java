df97435f9519153e55335d33ae2e73d9d6925537
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2008-12-22 20:32:07 +0000

    rearranging tests

--- trunk/source/src/test/java/com/test/junit/hibernate/CollectionTest.java
+++
@@ -1,9 +0,0 @@
-package com.test.junit.hibernate;
-
-import com.test.misc.HibernateBaseDAOTester;
-
-public class CollectionTest extends com.test.base.CollectionTest {
-	public void setHibernateBaseDAOTester(HibernateBaseDAOTester target) {
-		this.target = target;
-	}
-}
