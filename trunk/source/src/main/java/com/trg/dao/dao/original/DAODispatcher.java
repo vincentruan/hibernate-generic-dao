9f2ca286a83d796a5e6629b553174c1ce3acf4fd
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2008-12-23 15:57:55 +0000

    separated out base dispatcher for later use

--- trunk/source/src/main/java/com/trg/dao/dao/original/DAODispatcher.java
+++ trunk/source/src/main/java/com/trg/dao/dao/original/DAODispatcher.java
@@ -1,49 +1,17 @@
 package com.trg.dao.dao.original;
 
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
 
 import org.hibernate.NonUniqueResultException;
 
+import com.trg.dao.BaseDAODispatcher;
 import com.trg.dao.DAODispatcherException;
-import com.trg.dao.Util;
 import com.trg.dao.search.Search;
 import com.trg.dao.search.SearchResult;
 
 @SuppressWarnings("unchecked")
-public class DAODispatcher implements GeneralDAO {
-
-	private Map<String, Object> specificDAOs;
-
-	@Resource
-	private GeneralDAO generalDAO;
-
-	/**
-	 * In practice some DAOs could be put into this map using Spring. If a DAO
-	 * is in this map, it will be used instead of the general DAO. This provides
-	 * a way to override the default implementation for objects with special
-	 * considerations.
-	 */
-	public void setSpecificDAOs(Map<String, Object> specificDAOs) {
-		this.specificDAOs = specificDAOs;
-	}
-
-	/**
-	 * GeneralDAO has default implementations for the standard DAO methods.
-	 * Which model class it uses is specified when calling the particular
-	 * method.
-	 */
-	public void setGeneralDAO(GeneralDAO generalDAO) {
-		this.generalDAO = generalDAO;
-	}
-
-	protected Object getSpecificDAO(String className) {
-		return specificDAOs == null ? null : specificDAOs.get(className);
-	}
+public class DAODispatcher extends BaseDAODispatcher implements GeneralDAO {
 
 	public void create(Object object) {
 		Object specificDAO = getSpecificDAO(object.getClass().getName());
@@ -233,20 +201,6 @@ public class DAODispatcher implements GeneralDAO {
 			}
 		} else {
 			generalDAO.update(object);
-		}
-	}
-	
-	protected Object callMethod(Object specificDAO, String methodName, Object... args) {
-		try {
-			return Util.callMethod(specificDAO, methodName, args);
-		} catch (IllegalArgumentException e) {
-			throw new DAODispatcherException(e);
-		} catch (NoSuchMethodException e) {
-			throw new DAODispatcherException(e);
-		} catch (IllegalAccessException e) {
-			throw new DAODispatcherException(e);
-		} catch (InvocationTargetException e) {
-			throw new DAODispatcherException(e);
 		}
 	}
 }
