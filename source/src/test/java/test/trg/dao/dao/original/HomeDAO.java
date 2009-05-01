1dc4eec348eb2e759aecbac4d860341991174076
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-01 22:03:40 +0000

    Tagging 0.4.3

--- source/src/test/java/test/trg/dao/dao/original/HomeDAO.java
+++
@@ -1,9 +0,0 @@
-package test.trg.dao.dao.original;
-
-import test.trg.shared.model.Home;
-
-import com.trg.dao.dao.original.GenericDAO;
-
-public interface HomeDAO extends GenericDAO<Home, Long> {
-
-}
