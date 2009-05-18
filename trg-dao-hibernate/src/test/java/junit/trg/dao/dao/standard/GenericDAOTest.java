27ae0842d8f832e353c8b40e4c4b29e9a8721499
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-18 16:51:57 +0000

    Initial 0.5.0 import.

---
+++ trg-dao-hibernate/src/test/java/junit/trg/dao/dao/standard/GenericDAOTest.java
@@ -0,0 +1,193 @@
+package junit.trg.dao.dao.standard;
+
+import java.util.List;
+
+import org.hibernate.ObjectNotFoundException;
+
+import test.trg.BaseTest;
+import test.trg.dao.dao.standard.PersonDAO;
+import test.trg.dao.dao.standard.ProjectDAO;
+import test.trg.model.Person;
+import test.trg.model.Project;
+
+import com.trg.search.ExampleOptions;
+import com.trg.search.Search;
+import com.trg.search.Sort;
+
+public class GenericDAOTest extends BaseTest {
+
+	private PersonDAO personDAO;
+	
+	private ProjectDAO projectDAO;
+
+	public void setPersonDAO(PersonDAO personDAO) {
+		this.personDAO = personDAO;
+	}
+	
+	public void setProjectDAO(ProjectDAO projectDAO) {
+		this.projectDAO = projectDAO;
+	}
+
+
+	/**
+	 * Just quickly check that all the methods basically work. The underlying
+	 * implementation is more thoroughly tested in the
+	 * <code>junit.trg.dao.hibernate</code> package
+	 */
+	public void testDAO() {
+		Person fred = setup(new Person("Fred", "Smith", 35));
+		Person bob = setup(new Person("Bob", "Jones", 58));
+		Person cyndi = setup(new Person("Cyndi", "Loo", 58));
+		Person marty = setup(new Person("Marty", "McFly", 58));
+
+		
+		assertTrue(personDAO.save(fred));
+		assertTrue(personDAO.save(bob));
+		fred.setFather(bob);
+
+		assertEquals(bob, personDAO.find(bob.getId()));
+		assertEquals(fred, personDAO.find(fred.getId()));
+
+		//count
+		assertEquals(2, personDAO.count(new Search()));
+		assertEquals(2, personDAO.count(new Search(Person.class)));
+		
+		//searchAndCount
+		assertListEqual(new Person[] { bob, fred }, personDAO
+				.searchAndCount(new Search()).getResult());
+		assertListEqual(new Person[] { bob, fred }, personDAO
+				.searchAndCount(new Search(Person.class)).getResult());
+
+		//searchUnique
+		Search s = new Search();
+		s.addFilterEqual("id", bob.getId());
+		assertEquals(bob, personDAO.searchUnique(s));
+		s = new Search(Person.class);
+		s.addFilterEqual("id", bob.getId());
+		assertEquals(bob, personDAO.searchUnique(s));
+		
+		//searchGeneric
+		s = new Search();
+		s.addFilterEqual("id", bob.getId());
+		s.setResultMode(Search.RESULT_SINGLE);
+		s.addField("firstName");
+		assertEquals(bob.getFirstName(), personDAO.searchGeneric(s).get(0));
+		s.setSearchClass(Person.class);
+		assertEquals(bob.getFirstName(), personDAO.searchGeneric(s).get(0));
+
+		//searchUniqueGeneric
+		assertEquals(bob.getFirstName(), personDAO.searchUniqueGeneric(s));
+		s.setSearchClass(null);
+		assertEquals(bob.getFirstName(), personDAO.searchUniqueGeneric(s));
+		
+		//example
+		Person example = new Person();
+		example.setFirstName("Bob");
+		example.setLastName("Jones");
+		
+		s = new Search(Person.class);
+		s.addFilter(personDAO.getFilterFromExample(example));
+		assertEquals(bob, personDAO.searchUnique(s));
+		
+		example.setAge(0);
+		s.clear();
+		s.addFilter(personDAO.getFilterFromExample(example));
+		assertEquals(null, personDAO.searchUnique(s));
+		
+		s.clear();
+		s.addFilter(personDAO.getFilterFromExample(example, new ExampleOptions().setExcludeZeros(true)));
+		assertEquals(bob, personDAO.searchUnique(s));
+		
+		
+		assertTrue(personDAO.remove(bob));
+		assertEquals(null, personDAO.find(bob.getId()));
+
+		assertTrue(personDAO.removeById(fred.getId()));
+		assertEquals(null, personDAO.find(fred.getId()));
+
+		assertEquals(0, personDAO.count(new Search(Person.class)));
+
+		bob.setId(null);
+		fred.setId(null);
+
+		assertTrue(personDAO.save(bob));
+		assertTrue(personDAO.save(fred));
+		
+		personDAO.save(cyndi, marty);
+		for (Person p : personDAO.find(cyndi.getId(), bob.getId(), fred.getId())) {
+			assertNotNull(p);
+		}
+		
+		personDAO.removeByIds(cyndi.getId(), marty.getId());
+		personDAO.remove(cyndi, fred);
+		for (Person p : personDAO.find(cyndi.getId(), marty.getId(), fred.getId())) {
+			assertNull(p);
+		}
+		
+		flush();
+		clear();
+		
+		Person bob2 = copy(bob);
+		bob2.setFirstName("Bobby");
+		assertFalse(personDAO.save(bob2));
+
+		personDAO.flush();
+		
+		assertEquals("Bobby", personDAO.find(bob.getId()).getFirstName());
+		
+		
+		personDAO.refresh(bob2);
+		assertTrue(personDAO.isAttached(bob2));
+		assertFalse(personDAO.isAttached(bob));
+		
+		Person a = personDAO.getReference(bob2.getId());
+		Person b = personDAO.getReference(bob2.getId() + 10);
+		
+		Person[] pp = personDAO.getReferences(bob2.getId(), bob2.getId() + 10);
+		
+		assertEquals("Bobby", a.getFirstName());
+		assertEquals("Bobby", pp[0].getFirstName());
+		
+		try {
+			b.getFirstName();
+			fail("Entity does not exist, should throw error.");
+		} catch (ObjectNotFoundException ex) { }
+		try {
+			pp[1].getFirstName();
+			fail("Entity does not exist, should throw error.");
+		} catch (ObjectNotFoundException ex) { }
+	}
+	
+	/**
+	 * Test an example of adding and overriding DAO methods.
+	 */
+	public void testExtendingDAO() {
+		initDB();
+		
+		//two added methods...
+		
+		List<Project> expected = projectDAO.search(new Search().addFilterIn("name", "First", "Second"));
+		assertListEqual(projectDAO.findProjectsForMember(joeA), expected.toArray());
+		
+		assertListEqual(projectDAO.search(projectDAO.getProjectsForMemberSearch(joeA).addField("name")), "First", "Second");
+		
+		//overridden search method to deal with "duration"...
+		
+		Search s = new Search();
+		s.addFilterGreaterThan("duration", 50);
+		s.addSort(Sort.asc("duration"));
+		List<Project> results = projectDAO.search(s);
+		assertTrue(results.size() == 2);
+		assertEquals("Second", results.get(0).getName());
+		assertEquals("First", results.get(1).getName());
+		
+		s.clear();
+		s.addFilterLessThan("duration", 100);
+		s.addSort(Sort.desc("duration"));
+		results = projectDAO.search(s);
+		assertTrue(results.size() == 2);
+		assertEquals("Second", results.get(0).getName());
+		assertEquals("Third", results.get(1).getName());
+	}
+
+}
---
+++ trg-dao-hibernate/src/test/java/test/trg/dao/dao/standard/PersonService.java
@@ -0,0 +1,112 @@
+package test.trg.dao.dao.standard;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import test.trg.model.Person;
+
+import com.trg.search.ExampleOptions;
+import com.trg.search.Filter;
+import com.trg.search.Search;
+import com.trg.search.SearchResult;
+
+public class PersonService {
+
+	private PersonDAO dao;
+	
+	@Resource(name="personDAO")
+	public void setDAO(PersonDAO dao) {
+		this.dao = dao;
+	}
+	
+	
+	public int count(Search search) {
+		return dao.count(search);
+	}
+
+	public Person[] find(Long... ids) {
+		return dao.find(ids);
+	}
+
+	public Person find(Long id) {
+		return dao.find(id);
+	}
+
+	public List<Person> findAll() {
+		return dao.findAll();
+	}
+
+	public void flush() {
+		dao.flush();
+	}
+
+	public Person getReference(Long id) {
+		return dao.getReference(id);
+	}
+
+	public Person[] getReferences(Long... ids) {
+		return dao.getReferences(ids);
+	}
+
+	public boolean isAttached(Person entity) {
+		return dao.isAttached(entity);
+	}
+
+	public void refresh(Person entity) {
+		dao.refresh(entity);
+	}
+
+	public void remove(Person... entities) {
+		dao.remove(entities);
+	}
+
+	public boolean remove(Person entity) {
+		return dao.remove(entity);
+	}
+
+	public boolean removeById(Long id) {
+		return dao.removeById(id);
+	}
+
+	public void removeByIds(Long... ids) {
+		dao.removeByIds(ids);
+	}
+
+	public void save(Person... entities) {
+		dao.save(entities);
+	}
+
+	public boolean save(Person entity) {
+		return dao.save(entity);
+	}
+
+	public List<Person> search(Search search) {
+		return dao.search(search);
+	}
+
+	public SearchResult<Person> searchAndCount(Search search) {
+		return dao.searchAndCount(search);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List searchGeneric(Search search) {
+		return dao.searchGeneric(search);
+	}
+
+	public Person searchUnique(Search search) {
+		return dao.searchUnique(search);
+	}
+
+	public Object searchUniqueGeneric(Search search) {
+		return dao.searchUniqueGeneric(search);
+	}
+	
+	public Filter getFilterFromExample(Person example) {
+		return dao.getFilterFromExample(example);
+	}
+	
+	public Filter getFilterFromExample(Person example, ExampleOptions options) {
+		return dao.getFilterFromExample(example, options);
+	}
+}
