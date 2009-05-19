124b672307be8ced86fc086477b7cc91ade7c8c1
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-19 20:46:49 +0000

    renamed BaseGenericDAOImpl to BaseDAOImpl

--- trg-dao-hibernate/src/test/java/test/trg/dao/hibernate/dao/BaseDAOImpl.java
+++ trg-dao-hibernate/src/test/java/test/trg/dao/hibernate/dao/BaseDAOImpl.java
@@ -13,7 +13,7 @@ import com.trg.dao.hibernate.GenericDAOImpl;
  * Extension of GenericDAOImpl that is configured for Autowiring with Spring or J2EE.
  */
 @Repository
-public class BaseGenericDAOImpl<T, ID extends Serializable> extends GenericDAOImpl<T, ID> {
+public class BaseDAOImpl<T, ID extends Serializable> extends GenericDAOImpl<T, ID> {
 
 	@Override
 	@Resource
