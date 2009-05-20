7998322cd6070d7ff437e49002939f007aaa0d78
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-20 20:30:10 +0000

    fixed general and dispatcher test and dispatcher in regards to searching with generic result

--- trunk/trg-dao-hibernate/src/main/java/com/trg/dao/hibernate/original/DAODispatcher.java
+++ trunk/trg-dao-hibernate/src/main/java/com/trg/dao/hibernate/original/DAODispatcher.java
@@ -1,3 +1,17 @@
+/* Copyright 2009 The Revere Group
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.trg.dao.hibernate.original;
 
 import java.io.Serializable;
@@ -177,9 +191,17 @@ public class DAODispatcher extends BaseDAODispatcher implements GeneralDAO {
 		Object specificDAO = getSpecificDAO(search.getSearchClass().getName());
 		if (specificDAO != null) {
 			if (specificDAO instanceof GenericDAO) {
-				return ((GenericDAO) specificDAO).search(search);
+				return ((GenericDAO) specificDAO).searchGeneric(search);
 			} else {
-				return (List) callMethod(specificDAO, "search", search);
+				try {
+					return (List) callMethod(specificDAO, "searchGeneric", search);
+				} catch (DAODispatcherException ex) {
+					if (ex.getCause() instanceof NoSuchMethodException) {
+						return (List) callMethod(specificDAO, "search", search);
+					} else {
+						throw ex;
+					}
+				}
 			}
 		} else {
 			return generalDAO.search(search);
