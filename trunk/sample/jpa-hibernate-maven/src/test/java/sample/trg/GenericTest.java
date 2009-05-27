becd772b11c6f939f8e0eada0b855dfd1f8b20b4
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-27 15:20:04 +0000

    Initial import.

---
+++ trunk/sample/jpa-hibernate-maven/src/test/java/sample/trg/GenericTest.java
@@ -0,0 +1,11 @@
+package sample.trg;
+
+import junit.framework.Assert;
+
+public class GenericTest extends Assert {
+	
+	public void testOne() {
+		assertEquals(14, 14);
+		assertEquals(12, 19);
+	}
+}
