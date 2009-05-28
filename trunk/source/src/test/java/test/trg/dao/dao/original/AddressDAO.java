fa86cf9145a0ed962650fe9510862d69793c6561
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-28 17:44:56 +0000

    This hasn't been used since 0.4.2

--- trunk/source/src/test/java/test/trg/dao/dao/original/AddressDAO.java
+++
@@ -1,9 +0,0 @@
-package test.trg.dao.dao.original;
-
-import test.trg.shared.model.Address;
-
-import com.trg.dao.dao.original.GenericDAO;
-
-public interface AddressDAO extends GenericDAO<Address, Long> {
-
-}
