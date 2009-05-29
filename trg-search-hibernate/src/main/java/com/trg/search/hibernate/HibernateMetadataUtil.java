ffaaa89976cee2fdc82f6c9968ec511cbaf9de44
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2009-05-29 15:22:37 +0000

    removed unused import

--- trg-search-hibernate/src/main/java/com/trg/search/hibernate/HibernateMetadataUtil.java
+++ trg-search-hibernate/src/main/java/com/trg/search/hibernate/HibernateMetadataUtil.java
@@ -17,7 +17,6 @@ package com.trg.search.hibernate;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
