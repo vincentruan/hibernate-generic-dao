a5db8b44b63a89101db8e0eb05e494fe1655ce2f
dwolvert <dwolvert@8ef07cb6-1a53-0410-b69c-cfc34dde4201>
2010-04-10 17:02:00 +0000

    Repackaging to com.googlecode.genericdao

--- trunk/trg-search-hibernate/src/main/java/com/googlecode/genericdao/search/hibernate/HibernateSearchProcessor.java
+++ trunk/trg-search-hibernate/src/main/java/com/googlecode/genericdao/search/hibernate/HibernateSearchProcessor.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.trg.search.hibernate;
+package com.googlecode.genericdao.search.hibernate;
 
 import java.util.ArrayList;
 import java.util.Collection;
